c43a3702beb65888ae3727b76957aea1f8a146a2
lizati99 <medliz185@gmail.com>
2023-04-20 13:25:59 +0000

    Add Project folders

---
+++ Movies4All.Core/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using Movies4All.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movies4All.Core
+{
+    public interface IUnitOfWork:IDisposable
+    {
+        //IBaseRepository<Movie> Movies { get; }
+        int Complete();
+    }
+}
---
+++ Movies4All.Data/UnitOfWork.cs
@@ -0,0 +1,26 @@
+﻿using Movies4All.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movies4All.Data
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        public UnitOfWork()
+        {
+
+        }
+        public int Complete()
+        {
+            return 1;
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
